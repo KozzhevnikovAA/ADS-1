884ca17ce9b8263611502ea87221ceb1f47c6402
KozzhevnikovAA <116709158+KozzhevnikovAA@users.noreply.github.com>
2024-03-31 23:45:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,47 @@
 // Copyright 2022 NNTU-CS
+#include <iostream>
 #include <cstdint>
+#include <cmath>
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+for (uint64_t i = 2; i <= sqrt(value); i++) {
+if (value % i == 0)
+return false;
 }
+return true;
+}
+
+
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+int p = 0;
+int count = 0;
+while (count != n+1) {
+p+=1;
+if (checkPrime(p)) {
+count+=1;
 }
+}
+return p;
+  }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+uint64_t i = value+1;
+while (checkPrime(i) == false) {
+i+=1;
+}
+return i;
 }
 
+
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t count = 0;
+uint64_t i = 1;
+while (hbound > nextPrime(i)) {
+count+=nextPrime(i);
+i = nextPrime(i);
+}
+return count;
 }
